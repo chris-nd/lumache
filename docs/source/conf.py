af4df9ecb4da442529d6147cd42bafd08f1be0e0
Chris Nd <50488760+chris-nd@users.noreply.github.com>
2026-09-02 11:56:58 +0200

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,5 +35,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = ''
 html_static_path = ['_static']
